89432b18ffb95c1d7a0a32901cabf4c6b6cf8a81
abhaykalmegh <abhaykalmegh@gmail.com>
2022-12-05 15:46:25 +0530

    my second commit

---
+++ main.py
@@ -0,0 +1,86 @@
+from fastapi import FastAPI
+from enum import Enum
+from typing import Union
+
+app = FastAPI()
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get('/')
+async def root():
+    return {"Message": "I love my first Fast API project"}
+
+
+@app.get("/user/{id:int}")
+def users():
+    return {"Name": "Abhay Kalmegh",
+            "Gender": "Male",
+            "Email": "abhaykalmegh@gmail.com",
+            "Address": "Nagpur",
+            "status": "unmarried"
+            }
+
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: int):
+#     return {"item_id": item_id}
+
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+@app.get("/users")
+async def read_users():
+    return ["Rick", "Morty"]
+
+
+@app.get("/users")
+async def read_users2():
+    return ["Bean", "Elfo"]
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "restnet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+
+# @app.get("/items/{item_id}")
+# async def read_item(item_id: str, q: Union[str, None] = None):
+#     if q:
+#         return {"item_id": item_id, "q": q}
+#     return {"item_id": item_id}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
